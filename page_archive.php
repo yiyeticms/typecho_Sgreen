21cd3c6ede722aa0123a22bdb0e7875ed4d4c927
yiyeticms <admin@yiyeti.cc>
2017-03-20 22:32:12 +0800

    初次提交

--- page_archive.php
+++ page_archive.php
@@ -6,13 +6,15 @@
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
+
 <content>
-		<div class="main">
-			<div class="article">
-				<div class="article-title">
-				<?php $this->title() ?></div>
-				<div class="article-content">
-            <?php 
+  <div class="main">
+    <div class="article">
+      <div class="article-title">
+        <?php $this->title() ?>
+      </div>
+      <div class="article-content">
+        <?php 
                 $stat = Typecho_Widget::widget('Widget_Stat');
                 Typecho_Widget::widget('Widget_Contents_Post_Recent', 'pageSize='.$stat->publishedPostsNum)->to($archives);
                 $year=0; $mon=0; $i=0; $j=0;
@@ -22,7 +24,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
                     $mon_tmp = date('m',$archives->created);
                     $y=$year; $m=$mon;
                     if ($year > $year_tmp || $mon > $mon_tmp) {
-                        $output .= '</ul></div>';
+                        $output .= '</div>';
                     }
                     if ($year != $year_tmp || $mon != $mon_tmp) {
                         $year = $year_tmp;
@@ -31,14 +33,13 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
                     }
                     $output .= '<p>'.date('d日',$archives->created).' <a href="'.$archives->permalink .'">'. $archives->title .'</a></p>'; //输出文章
                 }
-                $output .= '</div></div></div>';
+                $output .= '</div></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div></div>';
                 echo $output;
             ?>
-        </div>
-</div>
-<div class="post-nav-comments">
-</div>
+      </div>
+    </div>
+    <?php $this->need('comments.php'); ?>
+  </div>
+  </div>
 </content>
-<?php $this->need('comments.php'); ?>
-</div>
 <?php $this->need('footer.php'); ?>
